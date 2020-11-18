4f440364a0db5121b153951897672b1ba9ce1685
Samuel Suter <smurtlesam@gmail.com>
2020-11-18 22:04:33 +0100

    patch: st-focus-20200731-43a395a

--- st.h
+++ st.h
@@ -81,6 +81,7 @@ typedef union {
 
 void die(const char *, ...);
 void redraw(void);
+void tfulldirt(void);
 void draw(void);
 
 void kscrolldown(const Arg *);
@@ -128,3 +129,4 @@ extern char *termname;
 extern unsigned int tabspaces;
 extern unsigned int defaultfg;
 extern unsigned int defaultbg;
+extern float alpha, alphaUnfocused;
